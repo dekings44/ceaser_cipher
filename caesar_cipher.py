6ff58a28b02f888d5c9fc516307ecfb4df352be1
Kingsley Ihemere <dekings44@gmail.com>
2022-05-28 19:21:54 +0100

    Enhance the Caesar cipher function and included the alphabet file

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,10 +1,10 @@
 
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 
-'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-'a', 'b', 'c', 'd', 'e', 'f', 'g', 
-'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
-]
+import art
+from alphabet import letters
+
+print(f'Welcome to\n\n {art.logo}')
+
 
 direction = input("Type 'encode' to encrypt, Type decode to decrypt\n")
 
